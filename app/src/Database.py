6e7381d6ab99ab3042b0877e0a343036d0c65276
lucas <development@waetzig.net>
2025-08-19 12:29:58 +0200

    ref(Database.py): changed way to initialize database service

--- app/src/Database.py
+++ app/src/Database.py
@@ -7,8 +7,15 @@ class Database:
     def __init__(
         self, user: str, password: str, host: str, port: int, database: str
     ) -> None:
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
+
+        # init SQLAlchemy engine
         self.engine = create_engine(
-            f"postgresql://{user}:{password}@{host}:{port}/{database}"
+            f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
         )
 
     def fetch_data(self, query: str) -> pd.DataFrame:
