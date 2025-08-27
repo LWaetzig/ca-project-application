8ffd7256166422234859c71e19e2e9c010984461
lucas <development@waetzig.net>
2025-08-27 22:09:18 +0200

    feat(entrypoint.py): refactor app structure and improve session state management

--- app/entrypoint.py
+++ app/entrypoint.py
@@ -1,23 +1,72 @@
-import streamlit as st
 import os
-from src.Database import Database
+
+import streamlit as st
+from dotenv import load_dotenv
+from src.utils import preload_table_content
+
+load_dotenv()
 
 
-def main():
+def app():
 
-    st.title("Open Discourse")
-
-    db = Database(
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "postgres"),
-        host=os.getenv("DB_HOST", "database"),
-        port=int(os.getenv("DB_PORT", "5432")),
-        database=os.getenv("DB_NAME", "next"),
+    st.set_page_config(
+        page_title="Analysing Bundestag Speeches",
+        layout="wide",
+        initial_sidebar_state="expanded",
     )
-    query = "SELECT count(*) FROM open_discourse.speeches WHERE electoral_term >= 17"
-    data = db.fetch_data(query)
-    st.write(data)
+
+    # Initialize session state for analysis
+    if "data_loaded" not in st.session_state:
+        st.session_state["data_loaded"] = False
+
+    if not st.session_state["data_loaded"]:
+        st.session_state["db_tables"] = [
+            "speeches",
+            "contributions",
+            "politicians",
+            "electoral_terms",
+            "factions",
+        ]
+
+        # Load data
+        for table in st.session_state["db_tables"]:
+            st.session_state[table] = preload_table_content(
+                os.path.join("data", f"{table}.parquet")
+            )
+
+        st.session_state["data_loaded"] = True
+
+    # init session state for chatbot
+    if "chatbot_messages" not in st.session_state:
+        st.session_state["chatbot_messages"] = [
+            {
+                "role": "assistant",
+                "content": "Hallo! Ich bin dein virtueller Assistent.",
+            }
+        ]
+
+    with st.sidebar:
+
+        pg = st.navigation(
+            {
+                "Home": [
+                    st.Page(os.path.join("pages", "home.py"), title="Home", icon="🏠")
+                ],
+                "Tools": [
+                    st.Page(
+                        os.path.join("pages", "analysis.py"),
+                        title="Analysis",
+                        icon="📊",
+                    ),
+                    st.Page(
+                        os.path.join("pages", "chatbot.py"), title="Chatbot", icon="🤖"
+                    ),
+                ],
+            }
+        )
+
+    pg.run()
 
 
 if __name__ == "__main__":
-    main()
+    app()
